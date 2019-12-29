38e1c65fc944cd3a479f4e0e9efa25022ac22481
linhao <245066003@qq.com>
2019-12-29 22:39:47 +0800

    fix:请求路径

--- vue-music/src/common/js/jsonp.js
+++ vue-music/src/common/js/jsonp.js
@@ -3,7 +3,7 @@ import originJSONP from 'jsonp'
 // 对外暴露方法
 export default function jsonp(url, data, option) {
   // 如果url中有问号的话就不加问号了,如果没有则加上
-  url += url.indexOf('?') < 0 ? '?' : '&' + param(data)
+  url += (url.indexOf('?') < 0 ? '?' : '&') + param(data)
   // 返回一个promise
   return new Promise((resolve, reject) => {
     // 调用jsonp方法
@@ -22,7 +22,7 @@ function param (data) {
   // 循环遍历data进行拼接
   for (const k in data) {
     let value = data[k] !== undefined ? data[k] : ''
-    url += `&${k}=${value}`
+    url += `&${k}=${encodeURIComponent(value)}`
   }
   // 去掉url中第一个&符号
   return url ? url.substring(1) : ''
